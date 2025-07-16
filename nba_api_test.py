57a530ecccb46ed5223cc951f348a2a31cd1c645
comnk <vhsiao36@gmail.com>
2025-07-15 22:32:54 -0700

    connected to nba api

---
+++ nba_api_test.py
@@ -0,0 +1,18 @@
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import drafthistory
+
+draft_df = drafthistory.DraftHistory()
+draft_df.get_json()
+
+# get_players returns a list of dictionaries, each representing a player.
+nba_players = players.get_players()
+
+big_fundamental = [
+    player for player in nba_players if player["full_name"] == "Tim Duncan"
+][0]
+big_fundamental
+
+draft = drafthistory.DraftHistory(season_year_nullable='2024') 
+draft_data = draft.get_data_frames()[0]
+print(draft_data)
